e658aa7d3d1d18faf6546d4759dd5e503396d15c
katunins <katunin.pavel@gmail.com>
2021-01-30 22:59:16 +0300

    правки

---
+++ database/migrations/2021_01_30_124915_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('master');
+            $table->float('time'); //min
+            $table->string('status'); //ready, finished, crashed
+            $table->string('mastercomment');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->string('info');
+            $table->string('deal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
