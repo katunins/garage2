133b7af2197585faa0b27776233e9f257f9bad3f
Pavel Katunin <katunin.pavel@gmail.com>
2021-02-21 23:50:15 +0300

    делаю Api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,5 +11,6 @@ class ApiController extends Controller
     {
         if ($request->has('password'))
             return response()->json(User::where('password', $request->code)->first(), 200);
+        else return response()->json(false, 400);
     }
 }
