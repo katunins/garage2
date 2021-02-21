84ee56b4ca32c167d4a6ac6145f927c84160399c
Pavel Katunin <katunin.pavel@gmail.com>
2021-02-22 00:21:26 +0300

    делаю Api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,7 +9,7 @@ class ApiController extends Controller
 {
     public function checkAuth(Request $request)
     {
-        if ($request->has('data'))
+        if ($request->has('data') && is_string($request->data))
             return response()->json(User::wherePassword((string)$request->data)->first(), 200);
         else return response()->json($request->all(), 400);
     }
