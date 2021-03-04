c1cccf54da287a87db7f62bf29f4c6888051c639
katunins <katunin.pavel@gmail.com>
2021-03-04 22:13:11 +0300

    обновляю интерфейс заполнения шаблонов

--- resources/views/newtemplate.blade.php
+++ resources/views/newtemplate.blade.php
@@ -11,9 +11,9 @@
 {{-- {{ dd($allParams) }} --}}
 {{-- сделаем скрытые параметры массива значений --}}
 @foreach ($allParams as $key=>$item)
-@foreach ($item as $i)
-<input class="all-params" type="hidden" name="{{ $key }}" value="{{ $i }}">
-@endforeach
+    @foreach ($item as $i)
+        <input class="all-params" type="hidden" name="{{ $key }}" value="{{ $i }}">
+    @endforeach
 @endforeach
 
 <h1>
@@ -34,17 +34,18 @@
     <input type="hidden" name="line" value="{{ $line }}">
     <input type="hidden" name="position" value="{{ $position }}">
     @isset ($template)
-    <input type="hidden" name="templateid" value="{{ $template->id }}">
+        <input type="hidden" name="templateid" value="{{ $template->id }}">
     @endisset
 
     <div class="input-blocks">
         <div class="input-group">
             @error ('taskname')
-            <p class="alert">{{ $message }}</p>
+                <p class="alert">{{ $message }}</p>
             @enderror
             <label for="taskname">Название задачи</label>
             <input class="max-size" type="text" id="taskname" name="taskname"
-                value="{{ old('taskname', $template->taskname ?? '') }}" placeholder="Подготовка картона">
+                value="{{ old('taskname', $template->taskname ?? '') }}"
+                placeholder="Подготовка картона">
 
             <br><br>
             <label> Мини-параметры</label>
@@ -66,7 +67,7 @@
     <div class="input-group">
         <label for="masters">ID мастеров</label>
         @error ('masters.*')
-        <p class="alert">{{ $message }}</p>
+            <p class="alert">{{ $message }}</p>
         @enderror
         <div class="param-input-help">
             @for ($i = 0; $i < 3; $i++) <div>
@@ -77,14 +78,15 @@
         @endfor
         <datalist id="masters">
             @foreach ($Users as $item)
-            <option value="{{ $item->id }}">{{ $item->name }}</option>
+                <option value="{{ $item->id }}">{{ $item->name }}</option>
             @endforeach
         </datalist>
     </div>
 
     <label for="taskidbefore">ID предыдущей задачи</label>
     <input class="mini-size" type="text" id="taskidbefore" name="taskidbefore"
-        value="{{ old('taskidbefore', $template->taskidbefore ?? '') }}" placeholder="123">
+        value="{{ old('taskidbefore', $template->taskidbefore ?? '') }}"
+        placeholder="123">
     <p class="help">Если задача должна быть поставлена после времени окончания задачи из другой линии, то
         укажите ID задачи из другой линии
         </div>
@@ -117,42 +119,56 @@
         @endfor
         <datalist id="conditions">
             @foreach ($allParams as $param=>$values)
-            <option value="{{ $param }}">
-                @endforeach
+                <option value="{{ $param }}">
+            @endforeach
         </datalist>
         <datalist id="equals">
             @foreach ([
-            'содержит'=>'=',
-            'не содержит'=>'!=',
-            // 'равно'=>'==',
-            // 'не равно'=>'!==',
-            'есть такой параметр'=>'?',
-            'нет такого параметра'=>'!?',
-            ] as $key=>$value)
-            <option value="{{ $value }}">{{ $key }}</option>
+                'содержит'=>'=',
+                'не содержит'=>'!=',
+                // 'равно'=>'==',
+                // 'не равно'=>'!==',
+                'есть такой параметр'=>'?',
+                'нет такого параметра'=>'!?',
+                ] as $key=>$value)
+                <option value="{{ $value }}">{{ $key }}</option>
             @endforeach
         </datalist>
         </div>
 
         <div class="input-group">
             @error ('producttime')
-            <p class="alert">{{ $message }}</p>
+                <p class="alert">{{ $message }}</p>
             @enderror
             <label for="time">Базовое время задачи, мин.</label>
             <input class="mini-size" type="text" id="producttime" name="producttime"
-                value="{{ old('producttime', $template->producttime ?? '') }}" placeholder="10">
+                value="{{ old('producttime', $template->producttime ?? '') }}"
+                placeholder="10" onchange="helpTimeCalc()">
+
             <p class="help">Время в минутах для выполнения задачи одного продукта не зависимо от формата и разворотов
             </p>
-            {{-- @error('paramtime')
-            <p class="alert">{{ $message }}
-    </p>
-    @enderror --}}
     <label for="time">Расчетное время задачи, сек.</label>
     <input class="mini-size" type="text" id="paramtime" name="paramtime"
-        value="{{ old('paramtime', $template->paramtime ?? '') }}" placeholder="0.5">
+        value="{{ old('paramtime', $template->paramtime ?? '') }}"
+        placeholder="0.5" onchange="helpTimeCalc()">
 
-    <p class="help">Время в секундах за квадратный дециметр площади. Если необходимо установить время меньше
-        секунды, то можно использовать десятичные числа с точнкой: 0.5, 0.25 сек.</p>
+    <p class="help">Время в секундах за квадратный дециметр площади. <br>
+        @foreach ([
+            ['name'=>'5 паспарту 16х21', 'square'=>3],
+            ['name'=>'Книга 15х15 10 разв', 'square'=>22],
+            ['name'=>'Книга 20х20 15 разв', 'square'=>60],
+            ['name'=>'Книга 30х30 20 разв', 'square'=>180],
+            ] as $item)
+            <li>
+                <span>{{ $item['name'] }} ({{ $item['square'] }} дм2) =
+                </span>
+                <span class="help-time-calc" data-square={{ $item['square'] }}>
+                    {{ ceil (($template->producttime ?? 0) + ($template->paramtime/60 ?? 0)* $item['square']) }}
+                </span>
+                 мин.
+            </li>
+        @endforeach
+    </p>
     <input class="check-box" type="checkbox" id="standarttemplate" name="standarttemplate" @if (old('standarttemplate',
         $template->standarttemplate ?? null)) checked @endif>
     <label style="display: inline" for="standarttemplate">Использовать шаблон как стандартный</label>
@@ -161,11 +177,12 @@
 
     <div class="input-group">
         @error ('buffer')
-        <p class="alert">{{ $message }}</p>
+            <p class="alert">{{ $message }}</p>
         @enderror
         <label for="buffer">Буферное время, мин.</label>
         <input class="mini-size" type="text" id="buffer" name="buffer"
-            value="{{ old('buffer', $template->buffer ?? '') }}" placeholder="3600">
+            value="{{ old('buffer', $template->buffer ?? '') }}"
+            placeholder="3600">
         <p class="help">Время, спустя которое будет поставлена следующая задача в конвеере. В каждую задачу также
             закладывается стандартный запасный период</p>
     </div>
@@ -174,24 +191,11 @@
 
         @for ($i = 0; $i < 2; $i++) <input class="mini-size" name="periods[{{ $i }}]" placeholder="Укажите период"
             value="{{ old('periods')[$i] ?? $template->periods[$i] ?? '' }}">
-            @endfor
+        @endfor
 
-            {{-- <div class="param-input-help">
-@for ($i = 0; $i < 3; $i++)
-                    <div>
-                        <input name="masters[{{ $i }}]" list="masters" placeholder="Выберете мастера"
-            value="{{ old('masters')[$i] ?? $template->masters[$i] ?? '' }}">
-            <button type="button" onclick="this.parentNode.querySelector('input').value=''">×</button>
-    </div> --}}
-
-    {{-- <input class="mini-size" type="text" name="period1"
-            value="{{ old('period1', $template->period1 ?? '') }}"
-    placeholder="9:00-11:00">
-    <input class="mini-size" type="text" name="period2" value="{{ old('period2', $template->period2 ?? '') }}"
-        placeholder="16:00-18:00"> --}}
-    <p class="help">Промежутки времени в которые нельзя планировать задачу. Перерыв на обед указывать не нужно,
-        как он учитывается по умолчанию. Пример 14:00-15:00 без пробелов!
-    </p>
+        <p class="help">Промежутки времени в которые нельзя планировать задачу. Перерыв на обед указывать не нужно,
+            как он учитывается по умолчанию. Пример 14:00-15:00 без пробелов!
+        </p>
     </div>
 
 
@@ -204,6 +208,16 @@
 
 </form>
 <script>
+
+    function helpTimeCalc() {
+        let productTime = Number(document.getElementById('producttime').value)
+        let paramTime = Number(document.getElementById('paramtime').value)
+        document.querySelectorAll('.help-time-calc').forEach(el=>{
+            console.log (el)
+            let square = Number(el.getAttribute('data-square'))
+            el.innerHTML = Math.round(productTime + paramTime/60*square)
+        })
+    }
     function setHelpValues(elemId) {
         let valuesArr = []
 
