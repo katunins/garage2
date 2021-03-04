951d907a2bb797aed6456946203c37a50e0d87ba
Pavel Katunin work <pavelkatuninhome>
2021-03-04 10:55:31 +0300

    правка

--- resources/views/newtemplate.blade.php
+++ resources/views/newtemplate.blade.php
@@ -10,10 +10,10 @@
 
 {{-- {{ dd($allParams) }} --}}
 {{-- сделаем скрытые параметры массива значений --}}
-@foreach($allParams as $key=>$item)
-    @foreach($item as $i)
-        <input class="all-params" type="hidden" name="{{ $key }}" value="{{ $i }}">
-    @endforeach
+@foreach ($allParams as $key=>$item)
+@foreach ($item as $i)
+<input class="all-params" type="hidden" name="{{ $key }}" value="{{ $i }}">
+@endforeach
 @endforeach
 
 <h1>
@@ -33,26 +33,25 @@
     <input type="hidden" name="productid" value="{{ $productId }}">
     <input type="hidden" name="line" value="{{ $line }}">
     <input type="hidden" name="position" value="{{ $position }}">
-    @isset($template)
-        <input type="hidden" name="templateid" value="{{ $template->id }}">
+    @isset ($template)
+    <input type="hidden" name="templateid" value="{{ $template->id }}">
     @endisset
 
     <div class="input-blocks">
         <div class="input-group">
-            @error('taskname')
-                <p class="alert">{{ $message }}</p>
+            @error ('taskname')
+            <p class="alert">{{ $message }}</p>
             @enderror
             <label for="taskname">Название задачи</label>
             <input class="max-size" type="text" id="taskname" name="taskname"
-                value="{{ old('taskname', $template->taskname ?? '') }}"
-                placeholder="Подготовка картона">
+                value="{{ old('taskname', $template->taskname ?? '') }}" placeholder="Подготовка картона">
 
             <br><br>
             <label> Мини-параметры</label>
 
 
             <div class="param-input-help">
-                @for($i = 0; $i < 3; $i++) <div>
+                @for ($i = 0; $i < 3; $i++) <div>
                     <input name="miniparams[{{ $i }}]" list="conditions" placeholder="Выберете параметр"
                         value="{{ old('miniparams')[$i] ?? $template->miniparams[$i] ?? '' }}">
                     <button type="button" onclick="this.parentNode.querySelector('input').value=''">×</button>
@@ -66,34 +65,33 @@
 
     <div class="input-group">
         <label for="masters">ID мастеров</label>
-        @error('masters.*')
-            <p class="alert">{{ $message }}</p>
+        @error ('masters.*')
+        <p class="alert">{{ $message }}</p>
         @enderror
         <div class="param-input-help">
-            @for($i = 0; $i < 3; $i++) <div>
+            @for ($i = 0; $i < 3; $i++) <div>
                 <input name="masters[{{ $i }}]" list="masters" placeholder="Выберете мастера"
                     value="{{ old('masters')[$i] ?? $template->masters[$i] ?? '' }}">
                 <button type="button" onclick="this.parentNode.querySelector('input').value=''">×</button>
         </div>
         @endfor
         <datalist id="masters">
-            @foreach($Users as $item)
-                <option value="{{ $item->id }}">{{ $item->name }}</option>
+            @foreach ($Users as $item)
+            <option value="{{ $item->id }}">{{ $item->name }}</option>
             @endforeach
         </datalist>
     </div>
 
     <label for="taskidbefore">ID предыдущей задачи</label>
     <input class="mini-size" type="text" id="taskidbefore" name="taskidbefore"
-        value="{{ old('taskidbefore', $template->taskidbefore ?? '') }}"
-        placeholder="123">
+        value="{{ old('taskidbefore', $template->taskidbefore ?? '') }}" placeholder="123">
     <p class="help">Если задача должна быть поставлена после времени окончания задачи из другой линии, то
         укажите ID задачи из другой линии
         </div>
 
         <div class="json-input-group">
             <label>Условия создания задачи</label>
-            @for($i = 0; $i < 3; $i++) <div>
+            @for ($i = 0; $i < 3; $i++) <div>
                 <div>
                     <input class="middle-size condition" name="conditions[{{ $i }}][condition]" list="conditions"
                         placeholder="Выберете параметр" onchange="setHelpValues('help-value-{{ $i }}')"
@@ -118,43 +116,41 @@
         </div>
         @endfor
         <datalist id="conditions">
-            @foreach($allParams as $param=>$values)
-                <option value="{{ $param }}">
-            @endforeach
+            @foreach ($allParams as $param=>$values)
+            <option value="{{ $param }}">
+                @endforeach
         </datalist>
         <datalist id="equals">
-            @foreach([
-                // 'содержит'=>'=',
-                // 'не содержит'=>'!=',
-                'равно'=>'==',
-                'не равно'=>'!==',
-                'есть такой параметр'=>'?',
-                'нет такого параметра'=>'!?',
-                ] as $key=>$value)
-                <option value="{{ $value }}">{{ $key }}</option>
+            @foreach ([
+            // 'содержит'=>'=',
+            // 'не содержит'=>'!=',
+            'равно'=>'==',
+            'не равно'=>'!==',
+            'есть такой параметр'=>'?',
+            'нет такого параметра'=>'!?',
+            ] as $key=>$value)
+            <option value="{{ $value }}">{{ $key }}</option>
             @endforeach
         </datalist>
         </div>
 
         <div class="input-group">
-            @error('producttime')
-                <p class="alert">{{ $message }}</p>
+            @error ('producttime')
+            <p class="alert">{{ $message }}</p>
             @enderror
             <label for="time">Базовое время задачи, мин.</label>
             <input class="mini-size" type="text" id="producttime" name="producttime"
-                value="{{ old('producttime', $template->producttime ?? '') }}"
-                placeholder="10">
+                value="{{ old('producttime', $template->producttime ?? '') }}" placeholder="10">
             <p class="help">Время в минутах для выполнения задачи одного продукта не зависимо от формата и разворотов
             </p>
             {{-- @error('paramtime')
             <p class="alert">{{ $message }}
     </p>
-    @enderror--}}
+    @enderror --}}
     <label for="time">Расчетное время задачи, сек.</label>
     <input class="mini-size" type="text" id="paramtime" name="paramtime"
-        value="{{ old('paramtime', $template->paramtime ?? '') }}"
-        placeholder="0.5">
-    <p id="pretime">10 мин</p>
+        value="{{ old('paramtime', $template->paramtime ?? '') }}" placeholder="0.5">
+
     <p class="help">Время в секундах за квадратный дециметр площади. Если необходимо установить время меньше
         секунды, то можно использовать десятичные числа с точнкой: 0.5, 0.25 сек.</p>
     <input class="check-box" type="checkbox" id="standarttemplate" name="standarttemplate" @if (old('standarttemplate',
@@ -164,37 +160,34 @@
     </div>
 
     <div class="input-group">
-        @error('buffer')
-            <p class="alert">{{ $message }}</p>
+        @error ('buffer')
+        <p class="alert">{{ $message }}</p>
         @enderror
         <label for="buffer">Буферное время, мин.</label>
         <input class="mini-size" type="text" id="buffer" name="buffer"
-            value="{{ old('buffer', $template->buffer ?? '') }}"
-            placeholder="3600">
+            value="{{ old('buffer', $template->buffer ?? '') }}" placeholder="3600">
         <p class="help">Время, спустя которое будет поставлена следующая задача в конвеере. В каждую задачу также
             закладывается стандартный запасный период</p>
     </div>
     <div class="json-input-group">
         <label>Запретные периоды времени</label>
 
-        <blade
-            for|%20(%24i%20%3D%200%3B%20%24i%20%3C%202%3B%20%24i%2B%2B)%20%3Cinput%20class%3D%26%2334%3Bmini-size%26%2334%3B%20name%3D%26%2334%3Bperiods%5B%7B%7B%2524i%7D%7D%5D%26%2334%3B%20placeholder%3D%26%2334%3B%D0%A3%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B8%D0%BE%D0%B4%26%2334%3B>
+        @for ($i = 0; $i < 2; $i++) <input class="mini-size" name="periods[{{ $i }}]" placeholder="Укажите период"
             value="{{ old('periods')[$i] ?? $template->periods[$i] ?? '' }}">
-        @endfor
+            @endfor
 
-        {{-- <div class="param-input-help">
-@for($i = 0; $i < 3; $i++)
+            {{-- <div class="param-input-help">
+@for ($i = 0; $i < 3; $i++)
                     <div>
                         <input name="masters[{{ $i }}]" list="masters" placeholder="Выберете мастера"
-        value="{{ old('masters')[$i] ?? $template->masters[$i] ?? '' }}">
-        <button type="button" onclick="this.parentNode.querySelector('input').value=''">×</button>
+            value="{{ old('masters')[$i] ?? $template->masters[$i] ?? '' }}">
+            <button type="button" onclick="this.parentNode.querySelector('input').value=''">×</button>
     </div> --}}
 
     {{-- <input class="mini-size" type="text" name="period1"
             value="{{ old('period1', $template->period1 ?? '') }}"
     placeholder="9:00-11:00">
-    <input class="mini-size" type="text" name="period2"
-        value="{{ old('period2', $template->period2 ?? '') }}"
+    <input class="mini-size" type="text" name="period2" value="{{ old('period2', $template->period2 ?? '') }}"
         placeholder="16:00-18:00"> --}}
     <p class="help">Промежутки времени в которые нельзя планировать задачу. Перерыв на обед указывать не нужно,
         как он учитывается по умолчанию. Пример 14:00-15:00 без пробелов!
