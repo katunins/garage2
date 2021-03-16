4ff6af0a9051fa94a508b7f56ed110521808c53a
Pavel Katunin work <pavelkatuninhome>
2021-03-16 17:42:36 +0300

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,43 +34,42 @@
             </form>
         </div>
         <div class="dashboard">
-            <div>
+            {{-- <div>
                 <h2>Сделки под угрозой срыва срока</h2>
                 <ul class="stuck-deals">
-                    @foreach ($Stuck as $item)
+@foreach ($Stuck as $item)
                             <li class="block-right-button task-status-{{ $item->type }}">
-                                <span
-                                    class="dealname">{{ $item->deal }}</span>
-                                <span>
-                                    Проблема в задаче {{ $item->task }}
-                                    <form action="/removestuck" method="get">
-                                        @csrf
-                                        <input type="hidden" name="stuckid" value="{{ $item->id }}">
-                                        <input class="right-button" type="submit" value="Удалить">
-                                    </form>
-                                </span>
-                            </li>
-                        @endforeach
-                </ul>
-            </div>
-            <div>
-                <h2>Просроченные задачи</h2>
-                <ul class="over-tasks">
-                    @foreach ($overTasks->sortBy('master') as $itemTask)
-                        <li class="task-status-{{ $itemTask->status }}"
-                            onclick="modalFromTask({{ json_encode($itemTask) }})">
-                            <span class="avatar"
-                                style="background-image: url({{ $Users->find($itemTask->master)->avatar ?? '' }})"></span>
-                            <span>{{ $itemTask->name }}</span>
-                            <span>{{ $itemTask->end }}</span>
-                            <span class="dealname">{{ $itemTask->deal }}</span>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+            <span class="dealname">{{ $item->deal }}</span>
+            <span>
+                Проблема в задаче {{ $item->task }}
+                <form action="/removestuck" method="get">
+                    @csrf
+                    <input type="hidden" name="stuckid" value="{{ $item->id }}">
+                    <input class="right-button" type="submit" value="Удалить">
+                </form>
+            </span>
+            </li>
+            @endforeach
+            </ul>
+        </div> --}}
+        <div>
+            <h2>Просроченные задачи</h2>
+            <ul class="over-tasks">
+                @foreach ($overTasks->sortBy('master') as $itemTask)
+                    <li class="task-status-{{ $itemTask->status }}"
+                        onclick="modalFromTask({{ json_encode($itemTask) }})">
+                        <span class="avatar"
+                            style="background-image: url({{ $Users->find($itemTask->master)->avatar ?? '' }})"></span>
+                        <span>{{ $itemTask->name }}</span>
+                        <span>{{ $itemTask->end }}</span>
+                        <span class="dealname">{{ $itemTask->deal }}</span>
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
+</div>
 
-    @include ('modal')
+@include ('modal')
 </div>
 <script src="js/general.js"></script>
