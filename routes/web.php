b0c1c94183b4ef4a51a483b12a90923349416f1c
Pavel Katunin work <pavelkatuninhome>
2021-03-16 17:46:24 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome')
         ->with('overTasks', CalendarController::getOverTasks())
-        ->with('Stuck', CalendarController::getStuck())
+        // ->with('Stuck', CalendarController::getStuck())
         ->with('Users', User::all());
 });
 
