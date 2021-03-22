b4572460de5ff523d6336ee4d5ca868313f7f686
Pavel Katunin work <pavelkatuninhome>
2021-03-22 11:42:31 +0300

    правки

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -117,134 +117,139 @@
     </div>
 </div>
 </div>
-@for ($day = 0; $day < $calendarDays; $day++)
-    @php
-        // Отфильтруем задачи на один день из недели, для случая если это календарь недели
-        $weekCalendarDay = clone $Date;
-        $weekCalendarDay->addDay($day);
-        $weekCalendarDay->setTime(0,0,1);
+<div class="calendar-container">
 
-        $weekEndTimeFilter = clone $weekCalendarDay;
-        $weekEndTimeFilter->setTime(23,59,59);
-
-        if ($calendarDays == 7)
-        {
-        echo '<br>
-        <div class="date-title">'.\App\Http\Controllers\CalendarController::getRusDate($weekCalendarDay).'</div>';
-        }
-    @endphp
-
-    <div class="calendar-block" @if ($calendarStyle==0)
-        style="grid-template-columns: 60px repeat({{ $Users->count() }}, 240px); grid-template-rows: 1fr repeat({{ $gridRowCount }}, {{ $scale }}px);"
-        @endif >
-
-        @if ($calendarStyle==0)
-            {{-- заголовок --}}
-            @foreach ($Users as $key => $item)
-                @php
-                    // пометим колонку определенным юзером
-                    $userColumn[$item->id] = ($key+2).'/'.($key+3);
-
-                @endphp
-                <div class="linehead" style="grid-row: 1/2; grid-column: {{ $userColumn[$item->id] }};">
-                    {{ $item->name }}
-                </div>
-            @endforeach
-        @endif
-        {{-- Задачи --}}
-
-        @foreach ($Tasks->whereBetween('start', [$weekCalendarDay, $weekEndTimeFilter])->sortBy('start') as $item)
-
-
-            @php
-
-                if ($calendarStyle!=0) $tasksToDelete[]=$item->id;
-            @endphp
-
-
-            <div class="task task-status-{{ $item->status }}" @if ($calendarStyle==0)
-                style="grid-row: {{ $item->startGrid }}/{{ $item->endGrid }}; grid-column: {{ $userColumn[$item->master] }}"
-            @else style="width: 600px; height: 40px;" @endif
-            draggable="true"
-            taskdata="{{ json_encode($item) }}"
-            onclick="modalFromTask(JSON.parse(this.getAttribute('taskdata')))">
-            <div @if ($calendarStyle!=0) style="margin-left: 25px" @endif class="title">
-                <span class="dealname">{{ $item->deal }}</span>
-                {{ $item->name }}
-                @if ($calendarStyle!=0)
-                    {{ $Users->find($item->master)->name }}
-                @endif
-            </div>
-            @if ($calendarStyle!=0)
-                <div class="avatar calendar-avatar"
-                    style="background-image: url({{ $Users->find($item->master)->avatar ?? '' }})">
-                </div>
-            @endif
-
-            <div @if ($calendarStyle!=0) style="margin-left: 25px" @endif class="taskname">
-                {{ $item->generalinfo }}
-            </div>
-    </div>
-    @if ($calendarStyle!=0) <input class="task-checkbox" type="checkbox" value="{{ $item->id }}" onchange="changeIDtodelete()"> @endif
-
-    @endforeach
-
-    @if ($calendarStyle==0)
-        {{-- Сетка --}}
-        {{-- начальная 1/2 линия --}}
+    @for ($day = 0; $day < $calendarDays; $day++)
         @php
-            $rowStart = 2;
-            $rowEnd =$gridInHour*$beforeAfter+2;
+            // Отфильтруем задачи на один день из недели, для случая если это календарь недели
+            $weekCalendarDay = clone $Date;
+            $weekCalendarDay->addDay($day);
+            $weekCalendarDay->setTime(0,0,1);
+
+            $weekEndTimeFilter = clone $weekCalendarDay;
+            $weekEndTimeFilter->setTime(23,59,59);
+
+            $columnCount=$Users->count()+2-1;
+
+            if ($calendarDays == 7)
+            {
+            echo '<br>
+            <div class="date-title">'.\App\Http\Controllers\CalendarController::getRusDate($weekCalendarDay).'</div>';
+            }
         @endphp
-        @for ($column = 1; $column < $Users->count()+2; $column++)
-            <div class="hour-line"
-                style="grid-row: {{ $rowStart }}/{{ $rowEnd }}; grid-column: {{ $column }}/{{ $column+1 }}">
-                @if ($column == 1)
-                    <div class="time-tag">
-                        {{ floor($workTimeStart-$beforeAfter) }} : 30
+
+        <div class="calendar-block" @if ($calendarStyle==0)
+            style="grid-template-columns: 60px repeat({{ $Users->count() }}, 240px); grid-template-rows: 1fr repeat({{ $gridRowCount }}, {{ $scale }}px);"
+            @endif >
+
+            @if ($calendarStyle==0)
+                {{-- заголовок --}}
+                @foreach ($Users as $key => $item)
+                    @php
+                        // пометим колонку определенным юзером
+
+                        if ($item->id === 9999) continue;
+                        $userColumn[$item->id] = ($key+2).'/'.($key+3);
+
+                    @endphp
+                    <div class="linehead" style="grid-row: 1/2; grid-column: {{ $userColumn[$item->id] }};">
+                        {{ $item->name }}
+                    </div>
+                @endforeach
+            @endif
+            {{-- Задачи --}}
+
+            @foreach ($Tasks->whereBetween('start', [$weekCalendarDay, $weekEndTimeFilter])->sortBy('start') as $item)
+
+
+                @php
+                    if ($calendarStyle!=0) $tasksToDelete[]=$item->id;
+                @endphp
+
+
+                <div class="task task-status-{{ $item->status }}" @if ($calendarStyle==0)
+                    style="grid-row: {{ $item->startGrid }}/{{ $item->endGrid }}; grid-column: {{ $userColumn[$item->master] }}"
+                @else style="width: 600px; height: 40px;" @endif
+                draggable="true"
+                taskdata="{{ json_encode($item) }}"
+                onclick="modalFromTask(JSON.parse(this.getAttribute('taskdata')))">
+                <div @if ($calendarStyle!=0) style="margin-left: 25px" @endif class="title">
+                    <span class="dealname">{{ $item->deal }}</span>
+                    {{ $item->name }}
+                    @if ($calendarStyle!=0)
+                        {{ $Users->find($item->master)->name }}
+                    @endif
+                </div>
+                @if ($calendarStyle!=0)
+                    <div class="avatar calendar-avatar"
+                        style="background-image: url({{ $Users->find($item->master)->avatar ?? '' }})">
                     </div>
                 @endif
-            </div>
-        @endfor
-        {{-- часовые ячейки --}}
-        @for ($row = 0; $row <= $workTimeEnd-$workTimeStart; $row ++)
+
+                <div @if ($calendarStyle!=0) style="margin-left: 25px" @endif class="taskname">
+                    {{ $item->generalinfo }}
+                </div>
+        </div>
+        @if ($calendarStyle!=0) <input class="task-checkbox" type="checkbox" value="{{ $item->id }}" onchange="changeIDtodelete()"> @endif
+
+        @endforeach
+
+        @if ($calendarStyle==0)
+            {{-- Сетка --}}
+            {{-- начальная 1/2 линия --}}
             @php
-                $rowStart=$gridInHour*$beforeAfter+2 +
-                $row*$gridInHour; $rowEnd=$gridInHour*$beforeAfter+2 + ($row+1)*$gridInHour;
+                $rowStart = 2;
+                $rowEnd =$gridInHour*$beforeAfter+2;
             @endphp
-            @for ($column=1; $column < $Users->count()+2; $column++)
-                <div class="hour-line clickable"
-                    style="grid-row: {{ $rowStart }}/{{ $rowEnd }}; grid-column: {{ $column }}/{{ $column+1 }}"
-                    hourData={{ $workTimeStart+$row }} userID={{ $column-1 }}
-                    dateData={{ $weekCalendarDay->format('Y-m-d') }}>
+            @for ($column = 1; $column < $columnCount; $column++)
+                <div class="hour-line"
+                    style="grid-row: {{ $rowStart }}/{{ $rowEnd }}; grid-column: {{ $column }}/{{ $column+1 }}">
                     @if ($column == 1)
                         <div class="time-tag">
-                            {{ $workTimeStart+$row }} : 00
+                            {{ floor($workTimeStart-$beforeAfter) }} : 30
                         </div>
                     @endif
                 </div>
             @endfor
-
-        @endfor
-
-        {{-- конечная 1/2 линия --}}
-        @php
-            $rowStart = $gridInHour*$beforeAfter+2 + ($row)*$gridInHour;
-            $rowEnd = $rowStart+$gridInHour/2;
-        @endphp
-        @for ($column = 1; $column < $Users->count()+2; $column++)
-            <div class="hour-line"
-                style="grid-row: {{ $rowStart }}/{{ $rowEnd }}; grid-column: {{ $column }}/{{ $column+1 }}">
-                @if ($column == 1)
-                    <div class="time-tag">
-                        {{ round($workTimeEnd) }} : 30
+            {{-- часовые ячейки --}}
+            @for ($row = 0; $row <= $workTimeEnd-$workTimeStart; $row ++)
+                @php
+                    $rowStart=$gridInHour*$beforeAfter+2 +
+                    $row*$gridInHour; $rowEnd=$gridInHour*$beforeAfter+2 + ($row+1)*$gridInHour;
+                @endphp
+                @for ($column=1; $column < $columnCount; $column++)
+                    <div class="hour-line clickable"
+                        style="grid-row: {{ $rowStart }}/{{ $rowEnd }}; grid-column: {{ $column }}/{{ $column+1 }}"
+                        hourData={{ $workTimeStart+$row }} userID={{ $column-1 }}
+                        dateData={{ $weekCalendarDay->format('Y-m-d') }}>
+                        @if ($column == 1)
+                            <div class="time-tag">
+                                {{ $workTimeStart+$row }} : 00
+                            </div>
+                        @endif
                     </div>
-                @endif
-            </div>
-        @endfor
-    @endif
+                @endfor
 
-    </div>
+            @endfor
+
+            {{-- конечная 1/2 линия --}}
+            @php
+                $rowStart = $gridInHour*$beforeAfter+2 + ($row)*$gridInHour;
+                $rowEnd = $rowStart+$gridInHour/2;
+            @endphp
+            @for ($column = 1; $column < $columnCount; $column++)
+                <div class="hour-line"
+                    style="grid-row: {{ $rowStart }}/{{ $rowEnd }}; grid-column: {{ $column }}/{{ $column+1 }}">
+                    @if ($column == 1)
+                        <div class="time-tag">
+                            {{ round($workTimeEnd) }} : 30
+                        </div>
+                    @endif
+                </div>
+            @endfor
+        @endif
+
+</div>
 
 @endfor
 
@@ -259,6 +264,7 @@
     </form>
 @endif
 
+</div>
 <div class="footer-block"></div>
 @include ('modal')
 <script src="js/general.js"></script>
@@ -271,14 +277,16 @@
             elem.setAttribute('data-checked', 0)
             elem.value = '   Временные'
             document.querySelectorAll('.task.task-' + elem.getAttribute('data-status')).forEach(el => {
-                el.classList.add('half-opacity')
+                // el.classList.add('half-opacity')
+                el.classList.add('hide')
             })
         } else {
             // включаем
             elem.setAttribute('data-checked', 1)
             elem.value = '✓ Временные'
             document.querySelectorAll('.task.task-' + elem.getAttribute('data-status')).forEach(el => {
-                if (el.classList.contains('half-opacity')) el.classList.remove('half-opacity')
+                // if (el.classList.contains('half-opacity')) el.classList.remove('half-opacity')
+                if (el.classList.contains('hide')) el.classList.remove('hide')
             })
         }
     }
